66b297432189e6ee2efb4a1b46395adcc3408700
Tafti <m.ahmadi.tafti@gmail.com>
2018-01-09 19:48:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -63,13 +63,15 @@ def processRequest(req):
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = "select wind from weather.forecast where woeid=2460286"
     yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
+    print("data2")
+    print(yql_url)
     result = urllib2.urlopen(yql_url).read()
     data = json.loads(result)
     
     
     
     
-    print(data)
+    
     res = makeWebhookResult(data)
     return res
 
