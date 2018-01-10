25fa2e1f86245ff953e06ab4073476192eee1ad5
Tafti <m.ahmadi.tafti@gmail.com>
2018-01-09 19:13:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -73,9 +73,9 @@ def processRequest(req):
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
+    city = parameters.get("location").get("city)
     if city is None:
-        print("no city")
+        print("nocity")
         return None
 
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='nome, ak')"
