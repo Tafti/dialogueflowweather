e2e92a80019d4a22acc8327de8bb8041174d68d2
Tafti <m.ahmadi.tafti@gmail.com>
2018-01-09 18:39:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def processRequest(req):
     yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
     result = urllib2.urlopen(yql_url).read()
     data = json.loads(result)
-    print data['query']['results']
+    print(int data['query']['results'])
     res = makeWebhookResult(data)
     return res
 
